252a6207a942e7586cabf0bf7b0ee2913eb3a1a7
kilcannon <scott.bouchey@gmail.com>
2018-06-05 10:21:34 -0700

    updated connection file

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,23 @@
 var mysql = require("mysql");
 
-var dbConfig = {
-    host: "localhost",
-    user: "root",
-    password: process.env.PASSWORD || "Eerste00$$",
-    database: "burgers_db"
-};
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL)
+}
+else {
+	connection = mysql.createConnection({
+		host: 'localhost',
+		user: 'root',
+		password: "Eerste00$$",
+		database: "burgers_db"
+	})
+}
+// var dbConfig = {
+//     host: "localhost",
+//     user: "root",
+//     password: process.env.PASSWORD || "Eerste00$$",
+//     database: "burgers_db"
+// };
 
-var connection = mysql.createPool(process.env.DATABASE_URL || dbConfig);
+// var connection = mysql.createPool(process.env.DATABASE_URL || dbConfig);
 
 module.exports = connection;
